e7b35e62f4eeef79102acc6a2df433c88afcd0a4
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-02-08 14:31:46 -0500

    initial commit

--- src/main/java/squier/john/hurtlocker/Main.java
+++ src/main/java/squier/john/hurtlocker/Main.java
@@ -1,5 +1,6 @@
+package squier.john.hurtlocker;
+
 import org.apache.commons.io.IOUtils;
-import java.io.IOException;
 
 public class Main {
 
@@ -12,6 +13,7 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
+
 
     }
 }
